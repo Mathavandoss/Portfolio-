e3889191b7c46606ec09e6c6b2397d4fe49a313d
Mathavandoss <148519482+Mathavandoss@users.noreply.github.com>
2025-07-18 13:02:42 +0530

    Update LoginPage.java

--- LoginPage.java
+++ LoginPage.java
@@ -3,15 +3,17 @@ package org.Testleaf;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+//creating class
 public class LoginPage
 {
+    //main method
     public static void main(String[] args)
     {
+        //driver launch
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com/v1/");
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
+        driver.get("https://www.saucedemo.com/v1/");//launching the browser
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");//sending username
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");//sending password
+        driver.findElement(By.id("login-button")).click();//clicking login button
     }
 }
